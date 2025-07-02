5c0290c48114777c91cdb8fb5dcb59c3785cb75d
Oleh Zinchenko <olegzinchenko1983@gmail.com>
2025-07-02 16:23:00 +0100

    added fetching menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter, RouterProvider } from 'react-router'
 
 import Home from './ui/Home'
-import Menu from './features/menu/Menu'
+import Menu, { loader as menuLoader } from './features/menu/Menu'
 import Cart from './features/cart/Cart'
 import CreateOrder from './features/order/CreateOrder'
 import Order from './features/order/Order'
@@ -11,26 +11,11 @@ const router = createBrowserRouter([
   {
     element: <AppLayout />,
     children: [
-      {
-        path: '/',
-        element: <Home />,
-      },
-      {
-        path: '/menu',
-        element: <Menu />,
-      },
-      {
-        path: '/cart',
-        element: <Cart />,
-      },
-      {
-        path: '/order/new',
-        element: <CreateOrder />,
-      },
-      {
-        path: '/order/:orderId',
-        element: <Order />,
-      },
+      { path: '/', element: <Home /> },
+      { path: '/menu', element: <Menu />, loader: menuLoader },
+      { path: '/cart', element: <Cart /> },
+      { path: '/order/new', element: <CreateOrder /> },
+      { path: '/order/:orderId', element: <Order /> },
     ],
   },
 ])
