2dcc25367ae9456cd5a4d4b3b4f7157ee1aba074
Oleh Zinchenko <olegzinchenko1983@gmail.com>
2025-07-03 20:16:42 +0100

    added fetching the individual order

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,29 @@ import Error from './ui/Error'
 import Menu, { loader as menuLoader } from './features/menu/Menu'
 import Cart from './features/cart/Cart'
 import CreateOrder from './features/order/CreateOrder'
-import Order from './features/order/Order'
+import Order, { loader as orderLoader } from './features/order/Order'
 import AppLayout from './ui/AppLayout'
 
 const router = createBrowserRouter([
   {
-    element: <AppLayout />,
+    Component: AppLayout,
     errorElement: <Error />,
     children: [
-      { path: '/', element: <Home /> },
+      { path: '/', Component: Home },
       {
         path: '/menu',
-        element: <Menu />,
+        Component: Menu,
         loader: menuLoader,
         errorElement: <Error />,
       },
-      { path: '/cart', element: <Cart /> },
-      { path: '/order/new', element: <CreateOrder /> },
-      { path: '/order/:orderId', element: <Order /> },
+      { path: '/cart', Component: Cart },
+      { path: '/order/new', Component: CreateOrder },
+      {
+        path: '/order/:orderId',
+        Component: Order,
+        loader: orderLoader,
+        errorElement: <Error />,
+      },
     ],
   },
 ])
