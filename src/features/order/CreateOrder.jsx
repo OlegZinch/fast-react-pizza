a885e0a5b1c480dbf60c99f82129aae9b2c3bbf5
Oleh Zinchenko <olegzinchenko1983@gmail.com>
2025-07-06 21:39:20 +0100

    added styling buttons

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -5,7 +5,7 @@ import { createOrder } from '../../services/apiRestaurant'
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
-    str
+    str,
   )
 
 const fakeCart = [
@@ -45,16 +45,16 @@ function CreateOrder() {
     <div>
       <h2>Ready to order? Let's go!</h2>
 
-      <Form method='POST'>
+      <Form method="POST">
         <div>
           <label>First Name</label>
-          <input type='text' name='customer' required />
+          <input type="text" name="customer" required />
         </div>
 
         <div>
           <label>Phone number</label>
           <div>
-            <input type='tel' name='phone' required />
+            <input type="tel" name="phone" required />
           </div>
           {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
@@ -62,24 +62,27 @@ function CreateOrder() {
         <div>
           <label>Address</label>
           <div>
-            <input type='text' name='address' required />
+            <input type="text" name="address" required />
           </div>
         </div>
 
         <div>
           <input
-            type='checkbox'
-            name='priority'
-            id='priority'
+            type="checkbox"
+            name="priority"
+            id="priority"
             // value={withPriority}
             // onChange={(e) => setWithPriority(e.target.checked)}
           />
-          <label htmlFor='priority'>Want to yo give your order priority?</label>
+          <label htmlFor="priority">Want to yo give your order priority?</label>
         </div>
 
         <div>
-          <input type='hidden' name='cart' value={JSON.stringify(cart)} />
-          <button disabled={isSubmiting}>
+          <input type="hidden" name="cart" value={JSON.stringify(cart)} />
+          <button
+            disabled={isSubmiting}
+            className="inline-block cursor-pointer rounded-full bg-yellow-400 px-4 py-3 font-semibold tracking-wide text-stone-800 uppercase transition-colors duration-300 hover:bg-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed"
+          >
             {isSubmiting ? 'Placing order...' : 'Order now'}
           </button>
         </div>
