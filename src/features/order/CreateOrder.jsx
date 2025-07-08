0423e28a9058e6a0d1620da3dcd6e41bc29808d9
Oleh Zinchenko <olegzinchenko1983@gmail.com>
2025-07-09 00:51:53 +0100

    styling the menu

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -81,7 +81,7 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <Button disabled={isSubmiting}>
+          <Button disabled={isSubmiting} type="primary">
             {isSubmiting ? 'Placing order...' : 'Order now'}
           </Button>
         </div>
