57effa66165081da233fdb3cc86c7a1838914511
Oleh Zinchenko <olegzinchenko1983@gmail.com>
2025-07-30 23:17:39 +0100

    install mui

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,3 +1,37 @@
+import Stack from '@mui/material/Stack'
+import { pink, green } from '@mui/material/colors'
+import SvgIcon from '@mui/material/SvgIcon'
+import { createSvgIcon } from '@mui/material/utils'
+
+import Icon from '@mui/material/Icon'
+import Button from '@mui/material/Button'
+
+function HomeIcon(props) {
+  return (
+    <SvgIcon {...props}>
+      <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
+    </SvgIcon>
+  )
+}
+
+const PlusIcon = createSvgIcon(
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="h-6 w-6"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M12 4.5v15m7.5-7.5h-15"
+    />
+  </svg>,
+  'Plus'
+)
+
 function Home() {
   return (
     <div>
@@ -6,6 +40,28 @@ function Home() {
         <br />
         Straight out of the oven, straight to you.
       </h1>
+      <Stack direction="row" spacing={3}>
+        <HomeIcon />
+        <HomeIcon color="primary" />
+        <HomeIcon color="secondary" />
+        <HomeIcon color="success" />
+        <HomeIcon color="action" />
+        <HomeIcon color="disabled" />
+        <HomeIcon sx={{ color: pink[500] }} />
+        {/* <HomeIcon fontSize="large" /> */}
+        <PlusIcon />
+        <PlusIcon color="secondary" />
+      </Stack>
+      <hr />
+      <Stack direction="row" spacing={3}>
+        <Icon>add_circle</Icon>
+        <Icon color="primary">add_circle</Icon>
+        <Icon sx={{ color: green[500] }}>add_circle</Icon>
+        <Icon fontSize="small">add_circle</Icon>
+        <Icon sx={{ fontSize: 30 }}>add_circle</Icon>
+      </Stack>
+      <hr />
+      <Button variant="contained">Hello world</Button>
     </div>
   )
 }
